c50057488e06854fcc8f370fa542f038bede58e7
Katarina Lindhed <katta36@hotmail.com>
2019-10-24 23:25:04 +0200

    Update myTestDBContext.cs

--- ReactCrudDemo/Models/myTestDBContext.cs
+++ ReactCrudDemo/Models/myTestDBContext.cs
@@ -22,7 +22,7 @@ namespace ReactCrudDemo.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                // To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=myTestDB;Integrated Security=True");
             }
         }
